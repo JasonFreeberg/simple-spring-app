6295685ee39927ae4d3c65a578378dfdd8571376
supadrasta <sudhakar_upadrasta@yahoo.com>
2021-12-26 12:58:01 +0530

    Deploy to dev slot
    Deploy to dev slot

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
     
     @GetMapping("/")
     public String home() {
-        return "Hello from Azure App Service )!";
+        return "Hello from Azure App Service to dev slot !";
     }
 }
